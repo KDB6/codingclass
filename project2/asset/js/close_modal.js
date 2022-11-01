78f1619d4b683060e973bf06dc329655fa8de2b3
KDB6 <ipkg7210@gmail.com>
2022-11-02 00:02:05 +0900

    11 01
    11 01

--- project2/asset/js/close_modal.js
+++
@@ -1,7 +0,0 @@
-const close_btn = document.querySelector(".close_btn");
-
-
-// <닫기 버튼>
-close_btn.innerHTML = `<a href="../board/boardMain.html">${close_btn.innerHTML}</a>`;
-
-
--- project3/asset/js/cancel_confirm.js
+++
@@ -1,6 +0,0 @@
-const boardBtn = document.querySelector(".board_btn");
-
-boardBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    boardBtn.alert("정말 삭제 할래?");
-});
